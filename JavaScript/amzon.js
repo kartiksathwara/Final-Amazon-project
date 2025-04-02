3f07e0720ba8a17452fdf0a7532a33cec568f896
Kartik Sathwara <kartiksathwara64@gmail.com>
2025-04-02 16:43:30 +0530

    Make the cart quantity interactive

--- JavaScript/amzon.js
+++ JavaScript/amzon.js
@@ -60,7 +60,7 @@ document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
-      
+
       let matchingItem;
 
       cart.forEach((item) => {
@@ -76,8 +76,15 @@ document.querySelectorAll('.js-add-to-cart')
           productId: productId,
           quantity: 1
         });
-
-        console.log(cart);
       }
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
     });
   });
