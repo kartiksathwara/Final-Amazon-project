edff76d6c2ce35087a0eea78ece75aa01cad5fa5
Kartik Sathwara <kartiksathwara64@gmail.com>
2025-04-07 11:47:10 +0530

    add a button above the add to cart button

--- JavaScript/amzon.js
+++ JavaScript/amzon.js
@@ -45,7 +45,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -76,5 +76,13 @@ document.querySelectorAll('.js-add-to-cart')
       const productId = button.dataset.productId;
       addtoCart(productId);
       updateCartQuantity();
+
+      const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+      
+      addedMessage.classList.add('added-to-cart-visible');
+
+      setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+      }, 2000);
     });
 });
