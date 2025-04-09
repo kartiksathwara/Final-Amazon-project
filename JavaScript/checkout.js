f9799602d38c04b93eaeaa0e8002a515e71c97de
Kartik Sathwara <kartiksathwara64@gmail.com>
2025-04-09 15:17:57 +0530

    Save data and generate HTML for payment summary

--- JavaScript/checkout.js
+++ JavaScript/checkout.js
@@ -1,3 +1,6 @@
 import { rendomOrderSummary } from "../JavaScript/checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+
 
 rendomOrderSummary();
+renderPaymentSummary();
