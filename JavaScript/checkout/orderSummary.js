79b99429a06ab6c131098de9d62304681e571ee7
Kartik Sathwara <kartiksathwara64@gmail.com>
2025-04-09 15:26:58 +0530

    Make payment summary interactive

--- JavaScript/checkout/orderSummary.js
+++ JavaScript/checkout/orderSummary.js
@@ -4,12 +4,9 @@ import { formatCurrency } from "../utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions,getDeliveryOption } from "../../data/deliveryoptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 
-hello();
 
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 export function rendomOrderSummary() {
 
@@ -126,6 +123,8 @@ export function rendomOrderSummary() {
 
                 const container = document.querySelector(`.js-cart-item-container-${productId}`);
                 container.remove();
+
+                renderPaymentSummary();
             });
         });
 
@@ -135,6 +134,7 @@ export function rendomOrderSummary() {
             const { productId, deliveryOptionId } = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId);
             rendomOrderSummary();
+            renderPaymentSummary();
          });
      });
 }
